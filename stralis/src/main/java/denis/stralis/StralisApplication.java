e407ae9dd588a9f5007b1eb4884fb8e0e434cc49
Denis Onder <denis_onder@protonmail.com>
2021-12-18 01:45:58 +0100

    init

---
+++ stralis/src/main/java/denis/stralis/StralisApplication.java
@@ -0,0 +1,13 @@
+package denis.stralis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StralisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StralisApplication.class, args);
+	}
+
+}
